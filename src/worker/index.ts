ca3f0d881878af600e3cd589ff3a19ae28763b49
hey-ak <akshay0127.be21@chitkara.edu.in>
2025-08-14 19:07:01 +0530

    fixed deployment error

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -9,6 +9,7 @@ import {
 } from "@getmocha/users-service/backend";
 import { DatabaseManager } from "@/shared/database";
 import { createDatabaseEndpoints } from "./database-endpoints";
+import type { Env } from "../../worker-configuration";
 
 const app = new Hono<{ Bindings: Env }>();
 
